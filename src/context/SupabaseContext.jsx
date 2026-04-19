f92cd46eafe678b1e8d5dca3620092ac6deb2a61
Arvin Raed <139760390+ghostm0nk@users.noreply.github.com>
2026-04-19 19:47:08 +0600

    Add src/context/SupabaseContext.jsx

---
+++ src/context/SupabaseContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useContext } from 'react';
+
+const SupabaseContext = createContext();
+
+export const useSupabase = () => useContext(SupabaseContext);
+
+export const SupabaseProvider = ({ value, children }) => {
+  return (
+    <SupabaseContext.Provider value={value}>
+      {children}
+    </SupabaseContext.Provider>
+  );
+};
